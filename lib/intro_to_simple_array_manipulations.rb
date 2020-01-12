40f6acd10726cfd818ea7b1ef56c14531cbf4c11
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-12 02:35:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,19 @@ end
 def pop_with_args(array)
   array.pop(2) 
 end
+
+def using_shift(array)
+  array.shift()
+end 
+
+def shift_with_args(array)
+  array.shift(2) 
+end
+
+array1 = [123, 456, 789]
+array2 = ["abc", "def", "ghi"]
+def using_concat(array1, array2)
+  array1.concat(array2)
+end 
+
+
